a6bf20300701aa3f7611dc83814a41322c2f325b
hongmaoxiao <buaaxhm@gmail.com>
2023-10-08 16:49:09 +0800

    Update readme.md

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ import React from 'react'
 
 export default function create(fn) {
   let listeners = []
+  // console.log('fn--------------------------------: ', fn)
   let state = {
     current: fn(
       merge => {
+        // console.log('merge--------before:', merge)
         if (typeof merge === 'function') {
           merge = merge(state.current)
         }
+        // console.log('merge--------after:', merge)
         state.current = { ...state.current, ...merge }
+        // console.log('state.current:', state.current)
+        // console.log('listeners:', listeners)
         listeners.forEach(listener => listener(state.current))
       },
       () => state.current
     ),
   }
+  // console.log('state:', state)
+  // console.log('listeners 111:', listeners)
   return [
     // useStore
     (selector, dependencies) => {
+      // console.log('selector 1111: ', selector)
       let selected = selector ? selector(state.current) : { ...state.current }
+      // console.log('selected 1111: ', selected)
       // Using functional initial b/c selected itself could be a function
       const [slice, set] = React.useState(() => selected)
       const sliceRef = React.useRef()
@@ -25,9 +34,15 @@ export default function create(fn) {
       React.useEffect(() => {
         const ping = () => {
           // Get fresh selected state
+          // console.log('selector 2222: ', selector)
           let selected = selector ? selector(state.current) : state.current
+          // console.log('typeof selected: ', typeof selected)
+          // console.log('sliceRef.current: ', sliceRef.current)
+          // console.log('selected 2222: ', selected)
+          // console.log('sliceRef.current !== selected: ', sliceRef.current !== selected)
           // If state is not a atomic shallow equal it
           if (sliceRef.current !== selected && typeof selected === 'object' && !Array.isArray(selected)) {
+            // console.log('in update here 111----: ', selected)
             selected = Object.entries(selected).reduce(
               (acc, [key, value]) => (sliceRef.current[key] !== value ? { ...acc, [key]: value } : acc),
               sliceRef.current
@@ -35,10 +50,12 @@ export default function create(fn) {
           }
           // Using functional initial b/c selected itself could be a function
           if (sliceRef.current !== selected) {
+            // console.log('in update here 222----: ', selected)
             // Refresh local slice
             set(() => selected)
           }
         }
+        // console.log('listeners 222:', listeners)
         listeners.push(ping)
         return () => (listeners = listeners.filter(i => i !== ping))
       }, dependencies || [selector])
