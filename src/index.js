cb04051dd984acb3d80e586f6fe6aa98363b98b3
hongmaoxiao <buaaxhm@gmail.com>
2023-10-09 11:43:50 +0800

    added  util from react-redux

--- src/index.js
+++ src/index.js
@@ -40,21 +40,8 @@ export default function create(fn) {
           // console.log('sliceRef.current: ', sliceRef.current)
           // console.log('selected 2222: ', selected)
           // console.log('sliceRef.current !== selected: ', sliceRef.current !== selected)
-          // If state is not a atomic shallow equal it
-          if (sliceRef.current !== selected && selected === Object(selected)) {
-            // console.log('in update here 111----: ', selected)
-            selected = Object.entries(selected).reduce(
-              (acc, [key, value]) =>
-                sliceRef.current[key] !== value
-                  ? Object.assign({}, acc, { [key]: value })
-                  : acc,
-              sliceRef.current
-            )
-          }
-          // Using functional initial b/c selected itself could be a function
-          if (sliceRef.current !== selected) {
-            // console.log('in update here 222----: ', selected)
-            // Refresh local slice
+          // Refresh local slice, functional initial b/c selected itself could be a function
+          if (!shallowEqual(sliceRef.current, selected)) {
             set(() => selected)
           }
         }
@@ -74,4 +61,36 @@ export default function create(fn) {
       destroy: () => ((state.listeners = []), (state.current = {})),
     },
   ]
+}
+
+const hasOwn = Object.prototype.hasOwnProperty
+function is(x, y) {
+  if (x === y) {
+    return x !== 0 || y !== 0 || 1 / x === 1 / y
+  }
+  return x !== x && y !== y
+}
+function shallowEqual(objA, objB) {
+  if (is(objA, objB)) {
+    return true
+  }
+  if (
+    typeof objA === 'object' ||
+    objA === null ||
+    typeof objB === 'object' ||
+    objB === null
+  ) {
+    return false
+  }
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+  for (let i = 0; i < keysA.length; i++) {
+    if (!hasOwn.call(objB, keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
+      return false
+    }
+  }
+  return true
 }
